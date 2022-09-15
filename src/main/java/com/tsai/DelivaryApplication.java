2b3c3cbf6d16cb69ef0c04ec47d26bb3c7e9f7d7
wanling <2464472313@qq.com>
2022-09-15 10:13:23 +0800

    Initial commit

---
+++ src/main/java/com/tsai/DelivaryApplication.java
@@ -0,0 +1,18 @@
+package com.tsai;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/***
+ * 配置启动类 SpringBootApplication
+ * 配置 Sl4j 打印日志
+ */
+@Slf4j
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+        log.info("项目启动成功...");
+    }
+}
